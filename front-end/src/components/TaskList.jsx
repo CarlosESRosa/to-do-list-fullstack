7c9147bfdc6ed9a762862d02d3fa95b163a62f51
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-07-03 21:53:48 -0300

    feat: :sparkles: add addTask feature

--- front-end/src/components/TaskList.jsx
+++ front-end/src/components/TaskList.jsx
@@ -1,17 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import myData from '../mockDB';
 
+const newtask = {
+  id: 4,
+  task: 'Jogar Vava',
+  status: 'pronto',
+  data: '03/06/2022',
+};
+
 function TaskList() {
   const [allTasks, setAllTasks] = useState([]);
-
   useEffect(() => {
     setAllTasks(myData);
-  });
+  }, []);
+
+  const addTask = (task) => {
+    setAllTasks([...allTasks, task]);
+  };
 
   return (
     <>
       <h1>ToDo List</h1>
       {allTasks.map((element) => <h1 key={element.id}>{element.task}</h1>)}
+      <button type="button" onClick={() => addTask(newtask)}>ADD</button>
     </>
   );
 }
