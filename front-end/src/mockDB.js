86a79403aab2c7e5a9e7717ec46c871fd9029e8b
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-07-03 21:24:11 -0300

    feat: :sparkles: add mockDB file

---
+++ front-end/src/mockDB.js
@@ -0,0 +1,16 @@
+const mockDB = [
+  {
+    id: 1,
+    task: 'Estudar React',
+  },
+  {
+    id: 2,
+    task: 'Estudar Node',
+  },
+  {
+    id: 3,
+    task: 'Jogar LOL',
+  },
+];
+
+export default mockDB;
