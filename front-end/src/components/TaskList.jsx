c41e07a58e6e771053e7842b7b511ac631b0fa79
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-07-04 15:41:56 -0300

    feat: :sparkles: done frontend initial setup

--- front-end/src/components/TaskList.jsx
+++ front-end/src/components/TaskList.jsx
@@ -1,28 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import myData from '../mockDB';
 
-const newtask = {
-  id: 4,
-  task: 'Jogar Vava',
-  status: 'pronto',
-  data: '03/06/2022',
-};
-
 function TaskList() {
   const [allTasks, setAllTasks] = useState([]);
   useEffect(() => {
     setAllTasks(myData);
   }, []);
 
-  const addTask = (task) => {
-    setAllTasks([...allTasks, task]);
+  const addTask = () => {
+    console.log('function to delete task');
+  };
+
+  const deleteTask = () => {
+    console.log('function to delete task');
+  };
+
+  const updateTask = () => {
+    console.log('function to update task');
   };
 
   return (
     <>
       <h1>ToDo List</h1>
-      {allTasks.map((element) => <h1 key={element.id}>{element.task}</h1>)}
-      <button type="button" onClick={() => addTask(newtask)}>ADD</button>
+      {allTasks.map((element) => (
+        <div key={element.id}>
+          <h1>{element.task}</h1>
+          <button type="button" onClick={() => deleteTask()}>DELETE</button>
+          <button type="button" onClick={() => updateTask()}>UPDATE</button>
+        </div>
+      ))}
+      <button type="button" onClick={() => addTask()}>ADD</button>
     </>
   );
 }
