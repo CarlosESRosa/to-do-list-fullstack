62f4775800213e620877e6a10ec0c6d2da7f723e
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-07-03 21:17:53 -0300

    feat: :sparkles: add TaskList file

---
+++ front-end/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskList from './components/TaskList';
+
+function App() {
+  return (
+    <div>
+      <TaskList />
+      <h1>Main</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/components/TaskList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TaskList() {
+  return (
+    <>
+      <h1>Task List</h1>
+      <h1>Task List</h1>
+    </>
+  );
+}
+
+export default TaskList;
