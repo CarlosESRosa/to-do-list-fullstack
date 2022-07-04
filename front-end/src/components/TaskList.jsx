e221c0f7de4a1b98c6ff30298240fc9fc31d4305
Carlos Eduardo <carlosrosa1001@gmail.com>
2022-07-03 21:49:19 -0300

    feat: :sparkles: start TaskList file

--- front-end/src/components/TaskList.jsx
+++ front-end/src/components/TaskList.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import myData from '../mockDB';
 
 function TaskList() {
+  const [allTasks, setAllTasks] = useState([]);
+
+  useEffect(() => {
+    setAllTasks(myData);
+  });
+
   return (
     <>
-      <h1>Task List</h1>
-      <h1>Task List</h1>
+      <h1>ToDo List</h1>
+      {allTasks.map((element) => <h1 key={element.id}>{element.task}</h1>)}
     </>
   );
 }
